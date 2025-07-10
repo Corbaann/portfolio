5e923b0195e8544858246d0a17588cf581a06c68
philip <mwihiaphilip7@gmail.com>
2025-07-10 13:50:12 +0300

    changed design

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
